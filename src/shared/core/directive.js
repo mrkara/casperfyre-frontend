f023498d699ff68583646f9c5b9610a05acf7620
ledgerleap1 <micky@ledgerleap.com>
2022-05-13 12:33:10 -0400

    update order status

--- src/shared/core/directive.js
+++ src/shared/core/directive.js
@@ -3,8 +3,9 @@ export const HistoryStatus = ({ data }) => {
   
   // Fulfilled = 0 means order placed, pending. “Pending” (regular color)
   // Fulfilled = 1 & success = 0 means order transfer has been sent. “Sent” (blue)
+  // Fulfilled = 1 & success = 1 means order is complete. “Confirmed” (green)
   // Fulfilled = 2 means order is failed. “Failed” (red)
-  // Fulfilled = 1 & success = 1 means order is complete. “Delivered” (green)
+  // others Cancelled
   const renderText = () => {
     if (+fulfilled === 0) {
       return <p style={{ fontSize: 'inherit' }}>Pending</p>;
@@ -13,12 +14,12 @@ export const HistoryStatus = ({ data }) => {
       return <p className='text-blue' style={{ fontSize: 'inherit' }}>Sent</p>;
     }
     if (+fulfilled === 1 && +success === 1) {
-      return <p className='text-success' style={{ fontSize: 'inherit' }}>Delivered</p>;
+      return <p className='text-success' style={{ fontSize: 'inherit' }}>Confirmed</p>;
     }
     if (+fulfilled === 2) {
       return <p className='text-primary' style={{ fontSize: 'inherit' }}>Failed</p>;
     }
-    return <p className='text-primary' style={{ fontSize: 'inherit' }}>Failed</p>;
+    return <p className='text-primary' style={{ fontSize: 'inherit' }}>Cancelled</p>;
   }
 
   return (
